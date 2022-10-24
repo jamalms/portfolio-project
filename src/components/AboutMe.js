f61ed9de353e79e32a70e640df9fc6835b521428
jamalms <95721607+jamalms@users.noreply.github.com>
2022-10-24 11:02:34 +0100

    Services Completed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,39 +1,32 @@
-import React from 'react'
-import author from '../me.jpeg';
+import React from "react";
+import author from "../me.jpg";
 
-function AboutMe() {
+const AboutMe = () => {
   return (
-    <div className="cintainer py-5">
-        <div className="row">
-            <div className="col-lg-6 col-mx-12">
-                <div className="photo-wrap mb-5">
-                <img className="profile-img" src={author} alt="author..."/>
-                </div>
-            </div>
-            <div className="col-lg-6 col-mx-12">
-            <h1 className="about-heading">About Me</h1>
-                <p>
-                <p>
-                Full Stack Developer | Node.js | React.js | Javascript | MySQL | Mongodb | Java | Python 
-Actively learning and improving skills in Machine Learning and Deep Learning applications.
-</p>
-Hello Everyone! I am Jamal Massalha, a meticulous and diligent professional new graduate from National College Of Ireland with a First Class Honours degree in Computing In Science. with hands-on experience in designing/developing multiple websites, applications, and various SQL databases.
-Throughout my career, I have remained a resourceful and results-driven professional with record of developing effective patient care plans in coordination with healthcare professionals.
-Excel at creating websites and user interfaces, using standard HTML / CSS practices.
-My area of expertise are as follows:
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12">
+          <div className="photo-wrap mb-5">
+            <img className="profile-img" src={author} alt="author..." />
+          </div>
+        </div>
+        <div className="col-lg-6 col-xm-12">
+          <h1 className="about-heading">about me</h1>
+          <p>
+            Hello! I am Jamal. I have been developing websites for over 5 years. I'm Full-Stack Web Developer. Technologies I use is MERN(MongoDB, Express, ReactJS and NodeJS).
 
-* Application Development
-* Web Development
-* Database Management
-* Software Architecture
-* Data Structure and Algorithms 
+            I create responsive websites that are displayed on all devices desktops and smartphones.
 
-I'm well renowned for performing comprehensive physical assessments and providing individualised patient care. Moreover, I am capable of analysing and modifying existing software while designing and evaluating end-user applications. Knowledgeable in utilising best and effective software development practices to write clear and well-tested code.
-I have the ability to incorporate data from back-end databases to automate integration and performance.
-Connect with me today or send me an email at jamalmssalha2@gmail.com if you want to discuss additional details regarding my work experience and the skills I have to offer
-                </p>
-            </div>
-        </div>    
+            Of course, before I begin developing any webapp, Landing Page, Business Website or E-commerce, I need to have a ready-made project layout (sketch).
+
+            And I’m ready to do this for you, before we start developing your website, we will discuss all the  details of your niche with you, I will conduct a survey of your competitors and make a list of their advantages and weaknesses.
+
+            All this is necessary to give your customers something that your competitors do not provide, or to present it too, but better!
+
+            After the website is finished and ready to deploy and sell a product or service. It is essential that your potential clients know about you. I will create an advertising campaign for you in  Google Adwords and Facebook!
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
