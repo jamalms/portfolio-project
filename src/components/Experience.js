0b2970bbe83c056b36a61c7332f83e2ec21ed543
jamalms <95721607+jamalms@users.noreply.github.com>
2022-10-25 10:52:44 +0100

    Experience component completed

---
+++ src/components/Experience.js
@@ -0,0 +1,95 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div className="experience">
+        <div className="d-flex justify-content-center my-5">
+            <h1>Experience</h1>
+        </div>
+        <div className="container experience-wrapper">
+            <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2015-2021</h3>
+                    <p>Critical Care RN,
+    Reuth Organization Full-time,
+    Apr 2015 - Aug 2021.
+    Tel Aviv-Yafo, Israel.</p>
+
+    <p>During my most recent role, Major duties included:
+    Providing exceptional guidance to patients / caregivers, collaborating with interdisciplinary team, overseeing daily caseload, maintaining high standards of individualised nursing care, and formulating treatment plans for patients.
+    A section of my key contribution include:</p>
+
+    <p>
+    -Resuscitated various patients suffering a heart failure by performing immediate CPR.<br />
+    -Developed health care plans for individuals and families while coordinating with physicians, case managers and social workers.<br />
+    -Started a patient education program with keen focus on providing information to patients and families regarding care in ICU.<br />
+    -Met and exceeded ongoing training needs by mentoring team of ICU nurses on a medical and critical care training program.<br />
+    -Facilitated students with health concerns and injuries by referring to another healthcare provider.<br />
+    -Conducted and completed comprehensive patient assessments to figure out individual needs.<br />
+    -Ensured delivery of continuous patient care by assigning multiple tasks to professional and paraprofessional nursing staff.<br />
+    -During my most recent role, Major duties included:<br /> 
+    Providing exceptional guidance to patients / caregivers, collaborating with interdisciplinary team, overseeing daily caseload, maintaining high standards of individualised nursing care, and formulating treatment plans for patients.<br />
+    <br />
+    -A section of my key contribution include:<br /> 
+    Resuscitated various patients suffering a heart failure by performing immediate CPR.<br />
+    Developed health care plans for individuals and families while coordinating with physicians, case managers and social workers.<br /> 
+    Started a patient education program with keen focus on providing information to patients and families regarding care in ICU.<br /> 
+    Met and exceeded ongoing training needs by mentoring team of ICU nurses on a medical and critical care training program.<br /> 
+    Facilitated students with health concerns and injuries by referring to another healthcare provider.<br /> 
+    Conducted and completed comprehensive patient assessments to figure out individual needs.<br /> 
+    Ensured delivery of continuous patient care by assigning multiple tasks to professional and paraprofessional nursing staff.<br />
+    <br />
+    <p>Skills: Communication · Respiratory rehabilitation · Critical Care Medicine · Electrocardiography (EKG) · Critical Care Nursing · Cardiac Rehabilitation · Mechanical Ventilation · Advanced Cardiac Life Support (ACLS)</p>
+</p>
+                </div>
+            </div>
+            {/* - */}
+            <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2021-Present</h3>
+                    <p>Student Software DeveloperStudent Software Developer
+Sep 2021 - Present · 1 yr 2 mosSep 2021 - Present · 1 yr 2 mos
+Project Name: Smart Surveillance / Security
+Distributed Systems Project:
+This project aims to design and develop distributed systems by creating a set of protocols / messages and building a reference implementation that simulates the operations of a smart, automated environment.
+• Created a cost-effective system to replace CCTV system. 
+• Implemented technology in conjunction with a number of devices and sensors to continuously monitor closed regions 24/7, without need for a large-scale system or costly equipment (CCTV). 
+Technology Used:
+For Service Definitions and implementations - gRPC (protos).
+For Naming Services - jmDNS.
+Client - Graphical User Interface (GUI)
+
+Project Name: iCARE
+The project aims to develop a web application for telemedicine to conduct health services remotely by developing multiple applications that collect measurements and do management remotely.
+• Created a Cardiac Application to track heart rate for patients with a high risk of developing arrhythmia.
+• Developed an Insulin Pump Application to regulate glucose monitoring and insulin infusion in response to glucose levels.
+• Designed a Ventilation Application with keen focus on assessing respiratory condition of mechanically ventilated patients by monitoring respiratory rate and oxygen saturation and directing requests to the ventilation machine.
+Technology Used:
+JavaScript | HTML | CSS | Angulary | Node.Js | Mongo Database | Google Maps API.Project Name: Smart Surveillance / Security Distributed Systems Project: This project aims to design and develop distributed systems by creating a set of protocols / messages and building a reference implementation that simulates the operations of a smart, automated environment. • Created a cost-effective system to replace CCTV system. • Implemented technology in conjunction with a number of devices and sensors to continuously monitor closed regions 24/7, without need for a large-scale system or costly equipment (CCTV). Technology Used: For Service Definitions and implementations - gRPC (protos). For Naming Services - jmDNS. Client - Graphical User Interface (GUI) Project Name: iCARE The project aims to develop a web application for telemedicine to conduct health services remotely by developing multiple applications that collect measurements and do management remotely. • Created a Cardiac Application to track heart rate for patients with a high risk of developing arrhythmia. • Developed an Insulin Pump Application to regulate glucose monitoring and insulin infusion in response to glucose levels. • Designed a Ventilation Application with keen focus on assessing respiratory condition of mechanically ventilated patients by monitoring respiratory rate and oxygen saturation and directing requests to the ventilation machine. Technology Used: JavaScript | HTML | CSS | Angulary | Node.Js | Mongo Database | Google Maps API.
+Computer Scince (Software Developer) StudenComputer Scince (Software Developer) Studen
+Sep 2021 - Sep 2022 · 1 yr 1 moSep 2021 - Sep 2022 · 1 yr 1 mo
+During my time over here, I built expertise in operating systems and administered UNIX based systems, MacOs, and windows systems, including server operating systems. Furthermore, I learned about various databases and created an SQL database for Businesses engaged in imports and exports.
+My achievements are as follows:
+
+Leveraged programming skills to design Clinic website.
+Developed various telemedicine apps, such as insulin pump, ventilation, and cardiac app.
+Compiled and executed clean, clear, and well-tested code for multiple projects.
+Designed client-side web applications in accordance with current and legacy web standards.
+Deployed web scripting language to manipulate and visualise data.
+Developed distributed systems by using gRPC framework.
+Enabled effective implementation of appropriate client-side frameworks and toolkits.
+Improved speed and search engines capabilities of written codes.
+Employed Grbc to create Smart Surveillance/Security System.
+Utilised SQL Server Reporting Services to generate reports.During my time over here, I built expertise in operating systems and administered UNIX based systems, MacOs, and windows systems, including server operating systems. Furthermore, I learned about various databases and created an SQL database for Businesses engaged in imports and exports. My achievements are as follows: Leveraged programming skills to design Clinic website. Developed various telemedicine apps, such as insulin pump, ventilation, and cardiac app. Compiled and executed clean, clear, and well-tested code for multiple projects. Designed client-side web applications in accordance with current and legacy web standards. Deployed web scripting language to manipulate and visualise data. Developed distributed systems by using gRPC framework. Enabled effective implementation of appropriate client-side frameworks and toolkits. Improved speed and search engines capabilities of written codes. Employed Grbc to create Smart Surveillance/Security System. Utilised SQL Server Reporting Services to generate reports.
+Skills: Linux terminal · Spring Boot · REST APIs · MongoDB · JSON · Software Development · JAVA Programmer · Node.js · React.js · Web Development · MySQL · gRPC · Core Java · JavaScript · HTML5 · Communication · algorithems and data structure (JAVA)
+                        </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
