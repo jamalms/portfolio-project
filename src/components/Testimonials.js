dc3395b175cd20d8ad160351229acff6e8a99c4d
jamalms <95721607+jamalms@users.noreply.github.com>
2022-10-27 21:50:03 +0100

    Testimonials Compenent Completed

---
+++ src/components/Testimonials.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TestimonialsCarousel from "./TestimonialsCarousel";
+
+function Testimonials() {
+  return (
+    <div className="testimonials">
+        <h1>my happy clients</h1>
+        <div className="testimonials-content">
+        <TestimonialsCarousel />
+
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
