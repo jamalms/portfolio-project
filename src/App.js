383a36a4001c2405a25677d6aa80c7515a1b1018
jamalms <95721607+jamalms@users.noreply.github.com>
2022-10-30 11:17:58 +0000

    The project is completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -41,7 +42,8 @@ function App() {
       <Experience />
       <Portfolio />
       <Testimonials />
-      <Contacts /> 
+      <Contacts />
+      <Footer />
     </>
   );
 }
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,7 +3,7 @@ import TestimonialsCarousel from "./TestimonialsCarousel";
 
 function Testimonials() {
   return (
-    <div className="testimonials">
+    <div id="testimonials" className="testimonials">
         <h1>my happy clients</h1>
         <div className="testimonials-content">
         <TestimonialsCarousel />
